87eba75e1aed8a4edf88c59c2291eaf994de69a3
yeehaha <h091155@nehs.hc.edu.tw>
2025-03-29 20:37:35 +0800

    Update midterm_project.ino

--- midterm_project/midterm_project.ino
+++ midterm_project/midterm_project.ino
@@ -11,6 +11,7 @@
 // for RFID
 #include <MFRC522.h>
 #include <SPI.h>
+using namespace std;
 
 /*===========================define pin & create module object================================*/
 // BlueTooth
@@ -21,18 +22,18 @@
 // 19    RX       <-  TX
 // TB6612, 請按照自己車上的接線寫入腳位(左右不一定要跟註解寫的一樣)
 // TODO: 請將腳位寫入下方
-#define MotorR_I1 2     // 定義 A1 接腳（右）
-#define MotorR_I2 3     // 定義 A2 接腳（右）
-#define MotorR_PWMR 13  // 定義 ENA (PWM調速) 接腳
-#define MotorL_I3 5     // 定義 B1 接腳（左）
-#define MotorL_I4 6     // 定義 B2 接腳（左）
-#define MotorL_PWML 12  // 定義 ENB (PWM調速) 接腳
+#define AIN1 2     // 定義 A1 接腳（右）
+#define AIN2 3     // 定義 A2 接腳（右）
+#define PWMA 13  // 定義 ENA (PWM調速) 接腳
+#define BIN1 5     // 定義 B1 接腳（左）
+#define BIN2 6     // 定義 B2 接腳（左）
+#define PWMB 12  // 定義 ENB (PWM調速) 接腳
 // 循線模組, 請按照自己車上的接線寫入腳位
-#define IRpin_LL 32
-#define IRpin_L 34
-#define IRpin_M 36
-#define IRpin_R 38
-#define IRpin_RR 40
+#define digitalPin1 32
+#define digitalPin2 34
+#define digitalPin3 36
+#define digitalPin4 38
+#define digitalPin5 40
 // RFID, 請按照自己車上的接線寫入腳位
 #define RST_PIN 9                 // 讀卡機的重置腳位
 #define SS_PIN 53                  // 晶片選擇腳位
@@ -51,18 +52,18 @@ void setup() {
     SPI.begin();
     mfrc522.PCD_Init();
     // TB6612 pin
-    pinMode(MotorR_I1, OUTPUT);
-    pinMode(MotorR_I2, OUTPUT);
-    pinMode(MotorL_I3, OUTPUT);
-    pinMode(MotorL_I4, OUTPUT);
-    pinMode(MotorL_PWML, OUTPUT);
-    pinMode(MotorR_PWMR, OUTPUT);
+    pinMode(AIN1, OUTPUT);
+    pinMode(AIN2, OUTPUT);
+    pinMode(BIN1, OUTPUT);
+    pinMode(BIN2, OUTPUT);
+    pinMode(PWMB, OUTPUT);
+    pinMode(PWMA, OUTPUT);
     // tracking pin
-    pinMode(IRpin_LL, INPUT);
-    pinMode(IRpin_L, INPUT);
-    pinMode(IRpin_M, INPUT);
-    pinMode(IRpin_R, INPUT);
-    pinMode(IRpin_RR, INPUT);
+    pinMode(digitalPin1, INPUT);
+    pinMode(digitalPin2, INPUT);
+    pinMode(digitalPin3, INPUT);
+    pinMode(digitalPin4, INPUT);
+    pinMode(digitalPin5, INPUT);
 #ifdef DEBUG
     Serial.println("Start!");
 #endif
@@ -71,16 +72,19 @@ void setup() {
 
 /*=====Import header files=====*/
 #include "RFID.h"
-#include "bluetooth.h"
 #include "node.h"
 #include "track.h"
+#include "bluetooth.h"
+#include "queue.h"
 /*=====Import header files=====*/
 
 /*===========================initialize variables===========================*/
-int l2 = 0, l1 = 0, m0 = 0, r1 = 0, r2 = 0;  // 紅外線模組的讀值(0->white,1->black)
-int _Tp = 90;                                // set your own value for motor power
+//int l2 = 0, l1 = 0, m0 = 0, r1 = 0, r2 = 0;  // 紅外線模組的讀值(0->white,1->black)
+int TP = 90;                                // set your own value for motor power
 bool state = false;     // set state to false to halt the car, set state to true to activate the car
+bool state2 = false;
 BT_CMD _cmd = NOTHING;  // enum for bluetooth message, reference in bluetooth.h line 2
+Queue<BT_CMD,100> commandQueue;
 /*===========================initialize variables===========================*/
 
 /*===========================declare function prototypes===========================*/
@@ -102,34 +106,65 @@ void loop() {
 
     if (uid) {  // If an RFID card is detected
         send_byte(uid,idSize);
-        delay(1000);
+        delay(100);
     }
 
     delay(100);  // Small delay to avoid excessive CPU usage
 }
 
-void SetState() {
-    // TODO:
-    _cmd=ask_BT();
 
-    //Serial.println(BT.available());
-    //delay(1000);
-    switch (_cmd) {
+
+void SetState() {
+    _cmd = ask_BT();
+    if (_cmd!=NOTHING){
+      state=true;
+      commandQueue.enqueue(_cmd);
+    }
+    
+}
+
+void Search() {
+    // TODO: let your car search graph(maze) according to bluetooth command from computer(python
+    // code)
+    // tracking(l2,l1,m,r1,r2);
+  int w2 = 1.1;
+  int w3 = 1.2;
+  int Tp = 80;
+  int Kp = Tp * 0.5;
+  int l3 = digitalRead(digitalPin1);
+  int l2 = digitalRead(digitalPin2);
+  int m = digitalRead(digitalPin3);
+  int r2 = digitalRead(digitalPin4);
+  int r3 = digitalRead(digitalPin5);
+
+  if (l2 && m && r2) {
+    while(l2!=0 && r2!=0){
+      MotorWriting(80,80);
+      l2 = digitalRead(digitalPin2);
+      r2 = digitalRead(digitalPin4);
+    }
+    BT_CMD cmd = commandQueue.peek();
+    Serial.println(cmd);
+    switch (cmd) {
         case FORWARD:
-            state = true;
-            MotorWriting(_Tp, _Tp);
+            //state = true;
+            MotorWriting(TP, TP);
+            delay(450);
             break;
         case BACKWARD:
-            state = true;
-            MotorWriting(-_Tp, -_Tp);
+            //state = true;
+            MotorWriting(-TP, TP);
+            delay(900);
             break;
         case LEFT:
-            state = true;
-            MotorWriting(-_Tp, _Tp);
+            //state = true;
+            MotorWriting(-TP, TP);
+            delay(450);
             break;
         case RIGHT:
-            state = true;
-            MotorWriting(_Tp, -_Tp);
+            //state = true;
+            MotorWriting(TP, -TP);
+            delay(450);
             break;
         case STOP:
             state = false;
@@ -137,15 +172,27 @@ void SetState() {
             break;
         default:
             break;
+      
     }
-    // 1. Get command from bluetooth
-    // 2. Change state if need
-}
-
-void Search() {
-    // TODO: let your car search graph(maze) according to bluetooth command from computer(python
-    // code)
-    // tracking(l2,l1,m,r1,r2);
+    commandQueue.dequeue();
+    
+  } else {
+    double error = (l3 * (-w3) + l2 * (-w2) + r2 * w2 + r3 * w3) / (l3 + l2 + m + r2 + r3);
+    double denominator = (l3 + l2 + m + r2 + r3);
+    if (denominator == 0) {
+      error = 0;  // 若所有感測器皆未觸發，則 error 設為 0
+    } else {
+      error = (l3 * (-w3) + l2 * (-w2) + r2 * w2 + r3 * w3) ;
+    }
+    int powerCorrection = Kp * error;  // ex. Kp = 100, 也與w2 & w3有關
+    int vR = Tp - powerCorrection;     // ex. Tp = 150, 也與w2 & w3有關
+    int vL = Tp + powerCorrection;
+    if (vR > 255) vR = 255;
+    if (vL > 255) vL = 255;
+    if (vR < -255) vR = -255;
+    if (vL < -255) vL = -255;
+    MotorWriting(vL, vR);  //Feedback to motors
+  }
 
 }
 /*===========================define function===========================*/
